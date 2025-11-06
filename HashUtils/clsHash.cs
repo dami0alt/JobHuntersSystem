4d2e893a403663d502a9ec4bcddcc62c9e1388fe
MartinaCanamares <martina.canamares@sarria.salesians.cat>
2025-11-06 19:40:46 +0100

    change name class

--- HashUtils/clsHash.cs
+++ HashUtils/clsHash.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace HashUtils
 {
-    public class Class1
+    public class clsHash
     {
+        private string CreateSalt()
+        {
+            string salt;
+            using(RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] bytes = new byte[32];
+                rng.GetBytes(bytes);
+                salt = BitConverter.ToString(bytes);
+            }
+            return salt;
+        }
+        public string CreatePassword(string pass)
+        {
+            string passHashed;
+            string saltHash = CreateSalt();
+            using(SHA256 hash = SHA256.Create())
+            {
+                byte[] bytes = hash.ComputeHash(Encoding.UTF8.GetBytes(pass + saltHash));
+                passHashed = BitConverter.ToString(bytes);
+            }
+            return passHashed;
+        }
+        public bool ValidatePass(string passwordUser, string passwordDB)
+        {
+            if(string.Equals(passwordUser, passwordDB))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
