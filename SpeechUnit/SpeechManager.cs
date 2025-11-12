985bca446f9b1991f2376152f99302a6cd6386b5
Axololt9597 <axel.fajardo@sarria.salesians.cat>
2025-11-12 16:37:27 +0100

    SpeechWithoutUser

---
+++ SpeechUnit/SpeechManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace SpeechUnit
+{
+    public class SpeechManager
+    {
+
+        private  SpeechRecognitionEngine recognizer;
+        private  SpeechSynthesizer synthesizer;
+        private  Dictionary<string, Action> commands;
+        private  Form parentForm;
+        private  string currentUser;
+
+        public SpeechManager(Form form, string user)
+        {
+            parentForm = form;
+            currentUser = user;
+
+            recognizer = new SpeechRecognitionEngine(new CultureInfo("en-US"));
+
+            InitializeCommands();
+            ConfigureRecognizer();
+        }
+
+        private void InitializeCommands()
+        {
+            commands = new Dictionary<string, Action>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "close", CloseApp },
+                { "exit", CloseApp },
+                { "time", ShowTime },
+                { "user info", ShowUserInfo }
+            };
+        }
+
+        private void ConfigureRecognizer()
+        {
+            Choices words = new Choices(commands.Keys.ToArray());
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Culture = recognizer.RecognizerInfo.Culture;
+            gb.Append(words);
+
+            Grammar grammar = new Grammar(gb);
+            grammar.Name = "SecureCoreVoiceCommands";
+            recognizer.LoadGrammarAsync(grammar);
+
+            recognizer.SetInputToDefaultAudioDevice();
+            recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+            recognizer.SpeechRecognitionRejected += Recognizer_SpeechRecognitionRejected;
+
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string command = e.Result.Text.ToLower();
+
+            if (commands.ContainsKey(command))
+            {
+                commands[command].Invoke();
+            }
+        }
+
+        private void Recognizer_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+        }
+
+        private void CloseApp()
+        {
+            parentForm.Invoke(new Action(() =>
+            {
+                parentForm.Close();
+                Application.Exit();
+            }));
+        }
+
+        private void ShowTime()
+        {
+            string hour = DateTime.Now.ToString("hh:mm tt");
+            parentForm.Invoke(new Action(() =>
+            {
+                MessageBox.Show($"Current time: {hour}", "System Time",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }));
+        }
+
+        private void ShowUserInfo()
+        {
+            parentForm.Invoke(new Action(() =>
+            {
+                MessageBox.Show($"User: {currentUser}\nAccess level: Administrator",
+                    "User Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }));
+        }
+
+        public void Stop()
+        {
+            recognizer.RecognizeAsyncStop();
+            recognizer.Dispose();
+        }
+    }
+}
