92647df2380ccef151708888fa966afc7449a491
Axololt9597 <axel.fajardo@sarria.salesians.cat>
2025-12-08 21:13:33 +0100

    update SpaceShipCategories

---
+++ SpaceShipCategories/FrmSpaceShipCategories.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceShipCategories
+{
+    public partial class FrmSpaceShipCategories : ManagementForms.frmBase
+    {
+        private string _tableName = "SpaceShipCategories";
+
+        public FrmSpaceShipCategories()
+        {
+            InitializeComponent();
+            base._tableName = _tableName;
+        }
+
+        protected override void ConfigurateDataGridView()
+        {
+            base.ConfigurateDataGridView();
+            dgtData.Columns["CodeSpaceShipCategory"].HeaderText = "Ship Code";
+            dgtData.Columns["DescSpaceShipCategory"].HeaderText = "Ship Description";
+
+            dgtData.Columns["idSpaceShipCategory"].Visible = false;
+        }
+    }
+}
+
