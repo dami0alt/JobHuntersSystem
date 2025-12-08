885fd5a18b1041f1572209dbb86f3f15f33be62c
MarCanLan <martina.canamares@sarria.salesians.cat>
2025-12-08 17:12:43 +0100

    feature: pantalla sectorCS

---
+++ Sectors/frmSectorsScreenCS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sectors
+{
+    public partial class frmSectorsScreenCS : ManagementForms.frmScreenCS
+    {
+        public frmSectorsScreenCS(string tableName, string formName, string controlId) : base(tableName, formName, controlId)
+        {
+            InitializeComponent();
+        }
+    }
+}
